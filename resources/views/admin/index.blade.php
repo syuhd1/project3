14e1f4fd8474f656c852ed787ed04fd52c471c84
Nurul Syuhada Binti Othman <nurulsyhda22@gmail.com>
2024-05-29 21:42:16 +0800

    admin template - seperate section

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,19 +1,29 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <!-- admin dashboard-->
-    <h1>Admin</h1>
+<html>
+  <head> 
+    @include('admin.css')
+  </head>
+  <body>
+  @include('admin.header')
 
-    <form method="POST" action="{{ route('logout') }}">
-                    @csrf
+  @include('admin.sidebar')
+      <!-- Sidebar Navigation end-->
+      <div class="page-content">
+        <div class="page-header">
+          <div class="container-fluid">
+          @include('admin.body')
 
-                    <input type="submit" value="Logout">
-                </form>
-    
-</body>
+        </div>   
+      </div>
+    </div>
+    <!-- JavaScript files-->
+    <script src="{{asset('admincss/vendor/jquery/jquery.min.js')}}"></script>
+    <script src="{{asset('admincss/vendor/popper.js/umd/popper.min.js')}}"> </script>
+    <script src="{{asset('admincss/vendor/bootstrap/js/bootstrap.min.js')}}"></script>
+    <script src="{{asset('admincss/vendor/jquery.cookie/jquery.cookie.js')}}"> </script>
+    <script src="{{asset('admincss/vendor/chart.js/Chart.min.js')}}"></script>
+    <script src="{{asset('admincss/vendor/jquery-validation/jquery.validate.min.js')}}"></script>
+    <script src="{{asset('admincss/js/charts-home.js')}}"></script>
+    <script src="{{asset('admincss/js/front.js')}}"></script>
+  </body>
 </html>
