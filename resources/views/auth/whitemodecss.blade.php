a4134156cc3af010c458c4b0666ae0dc511a1f5c
Nurul Syuhada Binti Othman <nurulsyhda22@gmail.com>
2024-06-12 16:48:21 +0800

    change of prod and satff pics, + whitemode css(not working)

---
+++ resources/views/auth/whitemodecss.blade.php
@@ -0,0 +1,43 @@
+<style>
+    /* .css */
+.light-mode {
+    background-color: #fff !important;
+    color: #000 !important;
+}
+
+/* thid one test, not sure */
+.box {
+            background-color: white; /* Change the background color to white */
+        }
+
+        /* Override dark mode styles */
+        body, .hero_area, form, .block {
+            background-color: #fff !important; /* Override background color */
+            color: #000 !important; /* Override text color */
+        }
+
+        .underline {
+            color: #000 !important; /* Override link color */
+        }
+
+        .dark\:text-gray-400 {
+            color: #666 !important; /* Override text color */
+        }
+
+        .dark\:hover\:text-gray-100:hover {
+            color: #000 !important; /* Override hover text color */
+        }
+
+        .dark\:focus\:ring-offset-gray-800 {
+            --tw-ring-offset-color: #fff !important; /* Override ring offset color */
+        }
+
+        .dark\:text-gray-400 {
+            color: #666 !important; /* Override secondary text color */
+        }
+
+        .dark\:focus\:ring-offset-gray-800:focus {
+            --tw-ring-offset-color: #fff !important; /* Override focus ring offset color */
+        }
+
+</style>
---
+++ resources/views/home/whitemodecss.blade.php
@@ -0,0 +1,8 @@
+<style>
+    /* custom.css */
+.light-mode {
+    background-color: #fff !important;
+    color: #000 !important;
+}
+
+</style>
