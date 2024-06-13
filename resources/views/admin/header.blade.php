ae93ac1f22abcd47bea6b28025e045bd93ac18ea
Nurul Syuhada Binti Othman <nurulsyhda22@gmail.com>
2024-06-14 02:06:46 +0800

    idk if there's error when try to logout admin

--- resources/views/admin/header.blade.php
+++ resources/views/admin/header.blade.php
@@ -14,7 +14,7 @@
         <div class="container-fluid d-flex align-items-center justify-content-between">
           <div class="navbar-header">
             <!-- Navbar Header--><a href="index.html" class="navbar-brand">
-              <div class="brand-text brand-big visible text-uppercase"><strong class="text-primary">Dark</strong><strong>Admin</strong></div>
+              <div class="brand-text brand-big visible text-uppercase"><strong class="text-primary">T-Hub: </strong><strong>Admin</strong></div>
               <div class="brand-text brand-sm"><strong class="text-primary">D</strong><strong>A</strong></div></a>
             <!-- Sidebar Toggle Btn-->
             <button class="sidebar-toggle"><i class="fa fa-long-arrow-left"></i></button>
--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -5,8 +5,8 @@
         <div class="sidebar-header d-flex align-items-center">
           <div class="avatar"><img src="{{asset('admincss/img/avatar-6.jpg')}}" alt="..." class="img-fluid rounded-circle"></div>
           <div class="title">
-            <h1 class="h5">Mark Stephen</h1>
-            <p>Web Designer</p>
+            <h1 class="h5" value="">Admin</h1>
+            <p>Admin</p>
           </div>
         </div>
         <!-- Sidebar Navidation Menus--><span class="heading">Main</span>
